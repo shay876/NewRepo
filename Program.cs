25e4618e7ff53d6ec6a3e5f2336b3eb42d035d82
shay876 <111309646+shay876@users.noreply.github.com>
2024-01-01 17:02:00 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //In the Main() method, create an object of data type Number and assign an amount to it
+            
+            Number number;
+
+            number.amount = 4.5m;
+           
+            //Print this amount to the console
+            Console.WriteLine(number.amount);
+        }
+
+
+        struct Number
+        {   //Create a struct called Number and give it the property “Amount” and have it be of data type decimal
+            public decimal amount;
+           
+        }
+    }
+}
